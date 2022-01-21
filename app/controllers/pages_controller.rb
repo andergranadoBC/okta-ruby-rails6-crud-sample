afc6591583cdd47fb470b8f2a29874ef587f2833
andergranadoBC <ander.granado@beclever.solutions>
2022-01-21 13:30:20 +0100

    Adds comments and cleans code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,16 @@ class PagesController < ApplicationController
   def home
     puts(session[:oktastate])
     @current_user = User.find_by(uid: session[:oktastate])
+    @groups = session[:groups]
+    @domain = session[:domain]
     
+    # Loads store info from session or matches the selected store from the session info
     @store = session[:store]
     if @store.nil?
       if session[:groups].include? params[:store]
         @store = params[:store]
       end
     end
-
-    @groups = session[:groups]
-    @domain = session[:domain]
   end
 
   def account
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    # def okta
-   def oktaoauth
-      @groups = request.env["omniauth.auth"]["extra"]["raw_info"]["groups"]
-      @domain = request.env["omniauth.auth"]['info']['email'].partition('@').last
+  # def okta
+  def oktaoauth
 
-      session[:groups] = @groups
-      session[:domain] = @domain
+    # Loads info received from Okta into Rails session 
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @groups = request.env["omniauth.auth"]["extra"]["raw_info"]["groups"]
+    @domain = request.env["omniauth.auth"]['info']['email'].partition('@').last
+    session[:oktastate] = request.env["omniauth.auth"]["uid"]
+    session[:groups] = @groups
+    session[:domain] = @domain
 
-      @customer_groups = Array.new
-      @groups.each do |group|
-         if group.starts_with?("CUSTOMER")
-            @customer_groups << group
-         end
+    # Checks the groups that starts with CUSTOMER to get all the stores accesible for that user
+    @customer_groups = Array.new
+    @groups.each do |group|
+      if group.starts_with?("CUSTOMER")
+        @customer_groups << group
       end
+    end
 
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-      session[:oktastate] = request.env["omniauth.auth"]["uid"]
-
-
-      if @customer_groups.count == 1
-         session[:store] = @customer_groups[0]
-         redirect_to root_path
-      else
-         redirect_to pages_client_selection_path
-      end
-   end
+    # If the user have only access to a single store, it redirects to that
+    # If the user belongs to different stores (staff users), are redirected to store selection pages
+    if @customer_groups.count == 1
+      session[:store] = @customer_groups[0]
+      redirect_to root_path
+    else
+      redirect_to pages_client_selection_path
+    end
+  end
 end
