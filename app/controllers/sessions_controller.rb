bfd366c04d007dbca061e0fc15353933141435ad
andergranadoBC <ander.granado@beclever.solutions>
2022-01-25 09:24:22 +0100

    Destroys session information when logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,13 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:oktastate] = nil
+    session[:groups] = nil
+    session[:domain] = nil
+    session[:store] = nil
     @current_user = session[:oktastate]
     @session = session[:oktastate]
+    @groups = session[:domain]
+    @store = session[:store]
     redirect_to root_path
   end
 end
